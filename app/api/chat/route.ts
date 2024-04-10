4288d369bbccc64b297def47ab78fc052f10a315
sumit maurya <sumit@almochat.com>
2024-04-10 13:36:38 +0530

    added a vercel url in chat/route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,10 +3,8 @@ const { APIChain } = require("langchain/chains");
 import { NextResponse } from 'next/server';
 
 export async function GET(request:Request,context:any) {
-  // const response = await axios.get("http://localhost:3001/api/getinfo");
-  // console.log(response.status,'axios response');
   try {
-    const OPEN_METEO_DOCS = `BASE URL: http://google.com`;
+    const OPEN_METEO_DOCS = `BASE URL: https://openai-database-jhnn.vercel.app/api/getinfo`;
   const model = new OpenAI({openAIApiKey:process.env.OPENAI_API_KEY ,modelName: "gpt-3.5-turbo-instruct" });
   const chain = APIChain.fromLLMAndAPIDocs(model, OPEN_METEO_DOCS, {
     headers: {
