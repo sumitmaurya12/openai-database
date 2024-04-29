7d6d2460d3a0d37a76a9470c474201e127403ceb
Sumit Maurya <sumit@almochat.com>
2024-04-29 16:46:18 +0530

    minor changes in route.ts to get the order id from url

--- app/api/getinfo/route.ts
+++ app/api/getinfo/route.ts
@@ -9,18 +9,35 @@ if (!url) throw new Error(`Expected env var SUPABASE_URL`);
 const client = createClient(url, privateKey);
 
 export async function GET(request:Request,context:any) {
+  const { searchParams } = new URL(request.url);
+  const orderIdParam = searchParams.get('orderId');
+    if (!orderIdParam) {
+      return NextResponse.json({
+        data:"No order details found"
+      });
+  }
+  const orderIds = orderIdParam.split(',');
         try {
-            const { data, error } = await client
-              .from('orders') // Replace 'orders' with your actual table name
-              .select('*')
-              .eq('order_id', 23599047)
-              .single(); // Fetch a single row
-            // console.log(data);
-            if (error) {
-                console.error('Error fetching order status:', error);
+          const orders = [];
+
+          for (const orderId of orderIds) {
+              const { data, error } = await client
+                  .from('orders') // Replace 'orders' with your actual table name
+                  .select('*')
+                  .eq('order_id', orderId)
+                  .single(); // Fetch a single row
+      
+              if (error) {
+                  console.error('Error fetching order status:', error);
+                  return NextResponse.json({
+                    error:error
+                  });
               }
+      
+              orders.push(data);
+          }
             return NextResponse.json({
-                data:data
+                data:orders
             })
     } catch (error) {
         console.log(error)
